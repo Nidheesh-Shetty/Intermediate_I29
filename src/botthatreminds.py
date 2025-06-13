1d690b5515042729136ee3dbf8c43e78209474c8
ILikeRobots0 <200047755+ILikeRobots0@users.noreply.github.com>
2025-06-13 19:18:04 +0800

    loaded data in botthatreminds.py
    wow i loaded the data i think

--- src/botthatreminds.py
+++ src/botthatreminds.py
@@ -1,2 +1,12 @@
-import json
-import pandas as pd
+import json #Importing json for the dataset 
+import pandas as pd 
+from datetime import datetime, timedelta #Importing datetime for the time in the dataset
+
+def load_medication(path="data/medication_schedule.json"): # I googled this, this helps to load the dataset
+  with open(path, "r") as f: #Reading the data
+    schedule = json.load(f)
+  return schedule
+
+def load_user(path="data/user_samples.csv"):
+  return pd.read_csv(path) #Reading the csv file
+
