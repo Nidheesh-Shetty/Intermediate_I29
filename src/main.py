aa34fa3858c93e3e76e97c635fed569b571cb369
ILikeRobots0 <200047755+ILikeRobots0@users.noreply.github.com>
2025-06-13 21:04:36 +0800

    Update main.py
    i got sidetracked

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from utilities import load_medication, load_user, check_medication #Getting stuff from utilities
+
+st.title("Medicine Reminder AI")
+
+schedule = load_medication("data/medication_schedule.json")
+user_logs = load_user("data/user_samples.csv")
+
 
