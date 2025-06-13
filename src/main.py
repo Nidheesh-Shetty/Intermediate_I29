058e882a4859154fa3317565317724c59324b053
ILikeRobots0 <200047755+ILikeRobots0@users.noreply.github.com>
2025-06-13 21:07:49 +0800

    Update main.py
    actually finished it

--- src/main.py
+++ src/main.py
@@ -6,4 +6,14 @@ st.title("Medicine Reminder AI")
 schedule = load_medication("data/medication_schedule.json")
 user_logs = load_user("data/user_samples.csv")
 
+user_id = "user_001" #Only works with 1 user :/
+time_window = 30 #Minutes
 
+due_meds = check_medication(schedule, user_logs, user_id=user_id, time_window=time_window)
+
+if due_meds: #Is this a boolean
+  st.subheader("Medications you need to take immediately:")
+  for med in due_meds:
+    st.markdown(f"- **{med}**")
+else:
+  st.success("You're all caught up on your medications for now :)")
