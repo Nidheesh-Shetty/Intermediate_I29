72fa4f952186407a0dc788648d9e6a787e4a7099
ILikeRobots0 <200047755+ILikeRobots0@users.noreply.github.com>
2025-06-13 20:28:14 +0800

    Update botthatreminds.py
    WOWOWOWO im done unless i have to debug this rubbish

--- src/botthatreminds.py
+++ src/botthatreminds.py
@@ -23,4 +23,13 @@ for x in schedule["medications"]:
   difference = abs((now-time).total_seconds()) / 60) #Seconds to minutes (/60)
   if difference <= time_window_minutes: #30
     today = now.strftime("%Y-%m-%d")
-  
+    filter = ( #just to make sure
+      (user_logs["user_id"]==user_id) &
+      (user_logs["medication_name"]==name) &
+      (user_logs["timestamp"].str.startswith(today_str))
+    )
+    today_logs = user_logs[filter]
+
+    if today_logs.empty or today_logs.iloc[-1]["taken"].lower() != "yes" #WOw this was hard, basically, if the logs are empty or latest one not taken yet, add it to the list
+      medications_due.append(name) 
+    return medications_due
